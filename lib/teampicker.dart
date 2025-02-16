219e668e36fd5ab0c17ebee144d5078ab34f223e
Raktbastr <raktbastr@proton.me>
2025-02-15 21:39:03 -0600

    Finished export feature!

--- lib/teampicker.dart
+++ lib/teampicker.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:to_csv/to_csv.dart' as exportCSV;
 
 void getData(String apiKey, String eventCode, Function(List<String>, List<String>) callback) async {
   final response = await http.get(
@@ -36,7 +37,6 @@ class TeamPicker extends StatefulWidget {
   _TeamPickerState createState() => _TeamPickerState();
 }
 
-
 class NotesPage extends StatefulWidget {
   final String teamCode;
   final String eventCode;
@@ -59,9 +59,10 @@ class NotesPage extends StatefulWidget {
 }
 
 class _NotesPageState extends State<NotesPage> {
-  final TextEditingController _controller1 = TextEditingController();
+  final TextEditingController _checkboxes = TextEditingController();
   final TextEditingController _controller2 = TextEditingController();
-  final TextEditingController _controller3 = TextEditingController();
+  final TextEditingController _switchvalue = TextEditingController();
+  double _slidervalue = 0.0;
 
   @override
   void initState() {
@@ -72,17 +73,19 @@ class _NotesPageState extends State<NotesPage> {
   _loadNotes() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
-      _controller1.text = prefs.getString('${widget.teamCode}_${widget.eventCode}_note1') ?? '';
+      _checkboxes.text = prefs.getString('${widget.teamCode}_${widget.eventCode}_note1') ?? '';
       _controller2.text = prefs.getString('${widget.teamCode}_${widget.eventCode}_note2') ?? '';
-      _controller3.text = prefs.getString('${widget.teamCode}_${widget.eventCode}_note3') ?? '';
+      _switchvalue.text = prefs.getString('${widget.teamCode}_${widget.eventCode}_note3') ?? '';
+      _slidervalue = double.tryParse(prefs.getString('${widget.teamCode}_${widget.eventCode}_note4') ?? '0.0') ?? 0.0;
     });
   }
 
   _saveNotes() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString('${widget.teamCode}_${widget.eventCode}_note1', _controller1.text);
+    await prefs.setString('${widget.teamCode}_${widget.eventCode}_note1', _checkboxes.text);
     await prefs.setString('${widget.teamCode}_${widget.eventCode}_note2', _controller2.text);
-    await prefs.setString('${widget.teamCode}_${widget.eventCode}_note3', _controller3.text);
+    await prefs.setString('${widget.teamCode}_${widget.eventCode}_note3', _switchvalue.text);
+    await prefs.setString('${widget.teamCode}_${widget.eventCode}_note4', _slidervalue.toString());
   }
 
   @override
@@ -100,18 +103,91 @@ class _NotesPageState extends State<NotesPage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          children: [
-            TextField(
-              controller: _controller1,
-              decoration: InputDecoration(labelText: 'Note 1'),
+            children: [
+            Text('Bot Starting Position', style: TextStyle(fontSize: 20)),
+            CheckboxListTile(
+              title: Text('Left'),
+              value: _checkboxes.text.contains('Left'),
+              onChanged: (bool? value) {
+              setState(() {
+                if (value == true) {
+                _checkboxes.text += 'Left ';
+                } else {
+                _checkboxes.text = _checkboxes.text.replaceAll('Left ', '');
+                }
+              });
+              },
+            ),
+            CheckboxListTile(
+              title: Text('Mid'),
+              value: _checkboxes.text.contains('Mid'),
+              onChanged: (bool? value) {
+              setState(() {
+                if (value == true) {
+                _checkboxes.text += 'Mid ';
+                } else {
+                _checkboxes.text = _checkboxes.text.replaceAll('Mid ', '');
+                }
+              });
+              },
+            ),
+            CheckboxListTile(
+              title: Text('Right'),
+              value: _checkboxes.text.contains('Right'),
+              onChanged: (bool? value) {
+              setState(() {
+                if (value == true) {
+                _checkboxes.text += 'Right ';
+                } else {
+                _checkboxes.text = _checkboxes.text.replaceAll('Right ', '');
+                }
+              });
+              },
             ),
             TextField(
               controller: _controller2,
-              decoration: InputDecoration(labelText: 'Note 2'),
+              decoration: InputDecoration(labelText: 'Auton Rundown'),
+              maxLines: null,
+              keyboardType: TextInputType.multiline,
             ),
-            TextField(
-              controller: _controller3,
-              decoration: InputDecoration(labelText: 'Note 3'),
+            CheckboxListTile(
+              title: Text('Can Score Alge'),
+              value: _switchvalue.text.contains('Can Score Alge'),
+              onChanged: (bool? value) {
+              setState(() {
+                if (value == true) {
+                _switchvalue.text = 'Yes ';
+                } else {
+                _switchvalue.text = '';
+                }
+              });
+              },
+            ),
+            CheckboxListTile(
+              title: Text('Cannot Score Alge'),
+              value: _switchvalue.text.contains('Cannot Score Alge'),
+              onChanged: (bool? value) {
+              setState(() {
+                if (value == true) {
+                _switchvalue.text = 'No ';
+                } else {
+                _switchvalue.text = '';
+                }
+              });
+              },
+            ),
+            Text('Coral Level', style: TextStyle(fontSize: 20)),
+            Slider(
+              value: _slidervalue,
+              onChanged: (double value) {
+              setState(() {
+                _slidervalue = value;
+              }); 
+              },
+              min: 0,
+              max: 3,
+              divisions: 3,
+              label: _slidervalue.round().toString(),
             ),
           ],
         ),
@@ -121,6 +197,7 @@ class _NotesPageState extends State<NotesPage> {
 }
 
 class _TeamPickerState extends State<TeamPicker> {
+  
   List<String> teamNames = [];
   List<String> teamCodes = [];
 
@@ -135,9 +212,30 @@ class _TeamPickerState extends State<TeamPicker> {
     });
   }
 
-  void _shareEvent() {
-    // Add your share logic here
-    print('Share button pressed');
+  Future<void> makeCSV() async {
+    
+    List<String> header = [];
+    header.add('Team Number');
+    header.add('Bot Position');
+    header.add('Auton Rundown');
+    header.add('Can Score Alge');
+    header.add('Coral Level');
+    List<List<String>> dataLists = [];
+    for (int i = 0; i < teamCodes.length; i++) {
+      List<String> data = [];
+      data.add(teamCodes[i]);
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String botPosition = prefs.getString('${teamCodes[i]}_${widget.eventCode}_note1') ?? '';
+      String autonRundown = prefs.getString('${teamCodes[i]}_${widget.eventCode}_note2') ?? '';
+      String canScoreAlge = prefs.getString('${teamCodes[i]}_${widget.eventCode}_note3') ?? '';
+      String coralLevel = prefs.getString('${teamCodes[i]}_${widget.eventCode}_note4') ?? '0.0';
+      data.add(botPosition);
+      data.add(autonRundown);
+      data.add(canScoreAlge);
+      data.add(coralLevel);
+      dataLists.add(data);
+    }
+    exportCSV.myCSV(header, dataLists, setHeadersInFirstRow: true, emptyRowsConfig: {1: 1}, fileName: '${widget.eventCode}-');
   }
 
   @override
@@ -150,7 +248,7 @@ class _TeamPickerState extends State<TeamPicker> {
         actions: [
           IconButton(
             icon: Icon(Icons.share),
-            onPressed: _shareEvent,
+            onPressed: makeCSV,
           ),
         ],
       ),
